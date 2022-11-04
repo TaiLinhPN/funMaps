6241aa760ed1dbdd57bdcfa44ce5afff8c2ae242
TaiLinh <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-11-04 17:38:10 +0700

    get region

--- Map.tsx
+++ Map.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-import {Float} from 'react-native/Libraries/Types/CodegenTypes';
+import { Float } from 'react-native/Libraries/Types/CodegenTypes';
+import GetLocation from 'react-native-get-location';
+
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
@@ -16,11 +18,7 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
 const Maps = () => {
-
-    
   interface Region {
     latitude: Float;
     longitude: Float;
@@ -29,23 +27,39 @@ const Maps = () => {
   }
 
   const initialRegion: Region = {
-    latitude: 37.78825,
-    longitude: -122.4324,
+    latitude: 16.0639,
+    longitude: 208.2456,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
-    };
-    
-   
-    
+  };
 
   const [region, setRegion] = useState<Region>(initialRegion);
+
+
+
+   GetLocation.getCurrentPosition({
+     enableHighAccuracy: true,
+     timeout: 15000,
+   })
+     .then(location => {
+      setRegion({
+        longitude: location.longitude,
+        latitude: location.latitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+     })
+     .catch(error => {
+       const {code, message} = error;
+       console.warn(code, message);
+     });
 
   const onRegionChange = (region: Region) => {
     setRegion(region);
   };
 
-    console.log(region);
-    
+  console.log(region);
+
   return (
     <View style={styles.container}>
       <MapView
